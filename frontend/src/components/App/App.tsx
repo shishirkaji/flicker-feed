fcf0c6c6a0b57df019dc3e652ff357d33e7f366d
Shishir Kaji Shrestha <shishir.shrestha@sine.co>
2022-05-09 09:24:20 +0930

    FLICK-16 added debounce search, decluttered the card rendering, implemented no result found.

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -7,62 +7,66 @@ import CardFrame from "../CardFrame/CardFrame";
 import Spinner from "../Spinner/Spinner";
 import { FlickerFeed } from "../common";
 import flickerService from "./../../services/flicker.service";
+import useDebounce from "../../customHooks/useDebounce";
 
 function App() {
-    const loadFlickerFeed = async (searchTerm?: string) => {
+    const [feeds, setFeeds] = useState<FlickerFeed[]>();
+    const [isLoading, setIsLoading] = useState(true);
+    const [search, setSearch] = useState<string>();
+    const debouncedSearchTerm = useDebounce(search || "", 500);
+
+    const showFlickerFeed = async (searchTerm?: string) => {
         const data = await flickerService.getFlickerFeeds(searchTerm);
         setFeeds(data);
         setIsLoading(false);
     };
 
-    const onSearchTermChange = async (newSearchTerm: string) => {
+    const onSearchTermChange = (newSearchTerm: string) => {
         setIsLoading(true);
         setSearch(newSearchTerm);
-        await loadFlickerFeed(newSearchTerm);
     };
-
-    const [feeds, setFeeds] = useState<FlickerFeed[]>();
-    const [isLoading, setIsLoading] = useState(true);
-    const [search, setSearch] = useState<string>();
 
     useEffect(() => {
         (async () => {
-            await loadFlickerFeed();
+            if (debouncedSearchTerm) {
+                await showFlickerFeed(debouncedSearchTerm);
+            } else {
+                await showFlickerFeed();
+            }
         })();
-    }, []);
+    }, [debouncedSearchTerm]);
 
+    const showFeeds = () => {
+        if (!feeds?.length) {
+            return <h1>No match found.</h1>;
+        }
+        return (
+            <>
+                <CardFrame>
+                    {feeds &&
+                        feeds.map((feed, index) => {
+                            return (
+                                <Card
+                                    key={feed.author + feed.imageUrl + index}
+                                    imageUrl={feed.imageUrl}
+                                    thumbNail={feed.thumbnailUrl}
+                                    title={feed.title}
+                                    tags={feed.tags.join(" ")}
+                                />
+                            );
+                        })}
+                </CardFrame>
+            </>
+        );
+    };
     return (
         <>
             <div className="App-frame" data-testid="AppFrame">
-                {isLoading ? (
-                    <Spinner />
-                ) : (
-                    <>
-                        <Search
-                            onSearchTermChange={onSearchTermChange}
-                            search={search}
-                        />
-
-                        <CardFrame>
-                            {feeds &&
-                                feeds.map((feed, index) => {
-                                    return (
-                                        <Card
-                                            key={
-                                                feed.author +
-                                                feed.imageUrl +
-                                                index
-                                            }
-                                            imageUrl={feed.imageUrl}
-                                            thumbNail={feed.thumbnailUrl}
-                                            title={feed.title}
-                                            tags={feed.tags.join(" ")}
-                                        />
-                                    );
-                                })}
-                        </CardFrame>
-                    </>
-                )}
+                <Search
+                    onSearchTermChange={onSearchTermChange}
+                    search={search}
+                />
+                {isLoading ? <Spinner /> : showFeeds()}
             </div>
         </>
     );
